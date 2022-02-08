495f839d5688c91d5c3f613a3fd1b2486a9afa46
Griffin Lamphere <7319002@isd535.org>
2022-02-08 10:39:58 -0600

    set up phaser

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
-// Put your JavaScript here
+let config = {
+    type: Phaser.Auto,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+}
+
+let game = new Phaser.Game(config);
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
