c999cd54e42653acf3c8c081b949025b7fd01a23
Lamp05 <7319002@isd535.org>
2022-02-11 02:42:52 -0600

    game complete, just needs finishing touches

--- scripts/main.js
+++ scripts/main.js
@@ -14,11 +14,16 @@ let cubeL = 20;
 let cubeX = canvas.width / 2 - (cubeL / 2);
 let cubeY = 0;
 
+//power up attributes
+let powL = 5;
+let powX = (Math.random() * canvas.width - powL);
+let powY = (Math.random() * canvas.width - powL);
+
 //player speed
 let rectSpeed = 5;
 
 //enemie speed
-let cubeSpeed = 2.5;
+let cubeSpeed = 1;
 
 let rightPressed = false;
 let leftPressed = false;
@@ -28,6 +33,8 @@ let downPressed = false;
 let score = 0;
 
 let lives = 3;
+
+let level = 1;
 
 //draws the rectangle the player controls
 function drawRectangle() {
@@ -49,6 +56,15 @@ function drawCube() {
   cubeY += cubeSpeed
 }
 
+//draws the power up
+function drawPow() {
+  ctx.beginPath();
+  ctx.rect(powX, powY, powL, powL);
+  ctx.fillStyle = "#2f4f4f";
+  ctx.fill();
+  ctx.closePath
+}
+
 //draws how many blocks the player has blocked from reaching the bottom
 function drawScore() {
   ctx.font = "20px Monospace";
@@ -67,7 +83,7 @@ function drawLives() {
 function drawLevel() {
   ctx.font = "20px Monospace";
   ctx.fillStyle = "#2f4f4f";
-  ctx.fillText("LEVEL:" + level, 10, 70)
+  ctx.fillText("LEVEL:" + level, 10, 80)
 }
 
 //the main draw function
@@ -102,60 +118,95 @@ function draw() {
     }
   }
 
-    drawRectangle();
+  drawRectangle();
 
-    drawCube();
+  drawCube();
 
-    drawScore();
+  drawPow();
 
-    drawLives();
+  drawScore();
 
-    //collision between cube and player
-    if (cubeX < rectX + rectL &&
-      cubeX + cubeL > rectX &&
-      cubeY < rectY + rectL &&
-      cubeL + cubeY > rectY) {
-        score += 1;
-      }
+  drawLives();
 
-    //if the enemy reaches the bottom, 1 life is removed
-    if (cubeY == canvas.height) {
-      lives -= 1;
-    }
+  drawLevel();
+
+  if //collision between cube and player
+    (cubeX < rectX + rectL &&
+    cubeX + cubeL > rectX &&
+    cubeY < rectY + rectL &&
+    cubeL + cubeY > rectY) {
+    score += 10;
+    level += 1;
+    cubeSpeed += .8;
+    cubeY = 0;
+    cubeX = (Math.random() * canvas.width - cubeL);
   }
 
-  //checks whether keys are pushed
-  function keyDownHandler(e) {
-    if (e.key == "Right" || e.key == "ArrowRight") {
-      rightPressed = true;
+  if //collision between player and power up. If the player touches the power up, they get 5 points and the cubeSpeed is reset to 1
+    (powX < rectX + rectL &&
+    powX + powL > rectX &&
+    powY < rectY + rectL &&
+    powL + powY > rectY) {
+    score += 5;
+    lives += 1;
+    powX = (Math.random() * canvas.width - powL);
+    powY = (Math.random() * canvas.height - powL);
     }
-    else if (e.key == "Left" || e.key == "ArrowLeft") {
-      leftPressed = true;
-    }
-    if (e.key == "up" || e.key == "ArrowUp") {
-      upPressed = true;
-    }
-    else if (e.key == "down" || e.key == "ArrowDown") {
-      downPressed = true;
-    }
+
+  //if the enemy reaches the floor, the player loses a life and the cube is reset
+  if (cubeY > canvas.height) {
+    cubeY = 0;
+    cubeX = (Math.random() * canvas.width - cubeL);
+    lives -= 1;
   }
 
-  function keyUpHandler(e) {
-    if (e.key == "Right" || e.key == "ArrowRight") {
-      rightPressed = false;
-    }
-    else if (e.key == "Left" || e.key == "ArrowLeft") {
-      leftPressed = false;
-    }
-    if (e.key == "up" || e.key == "ArrowUp") {
-      upPressed = false;
-    }
-    else if (e.key == "down" || e.key == "ArrowDown") {
-      downPressed = false;
-    }
+  //if the player has 0 lives, the game resets and "game over" is displayed
+  if (lives == 0) {
+    document.location.reload(); alert("GAME OVER");
   }
 
-  document.addEventListener("keydown", keyDownHandler, false);
-  document.addEventListener("keyup", keyUpHandler, false);
+  if (cubeY < 0) {
+    document.location.reload(); alert("GAME OVER")
+  }
 
-  let interval = setInterval(draw, 10);
+  if (level == 10) {
+    document.location.reload(); alert("GAME WON, SCORE:" + score)
+  }
+}
+
+//checks wether keys are pushed
+function keyDownHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  }
+  else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
+  if (e.key == "up" || e.key == "ArrowUp") {
+    upPressed = true;
+  }
+  else if (e.key == "down" || e.key == "ArrowDown") {
+    downPressed = true;
+  }
+}
+
+//checks wether keys are not pushed
+function keyUpHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  }
+  else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  }
+  if (e.key == "up" || e.key == "ArrowUp") {
+    upPressed = false;
+  }
+  else if (e.key == "down" || e.key == "ArrowDown") {
+    downPressed = false;
+  }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+let interval = setInterval(draw, 10);
